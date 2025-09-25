42f2a8cd2b109e6fde44794a314d47a917fe366b
DominicDev <s0l0estudi4r@gmail.com>
2025-09-25 20:05:48 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HELLO FIASCO';
+export const nameSongs = [
+	"BEFORE TIME LEAVES YOU ", /*1*/
+	"GORGEOUS GIRL", /*2*/
+	"APPLES AND SODA POP", /*3*/
+	"HOLD ME CLOSE", /*4*/
+	"CHESS", /*5*/
+	"TRUST", /*6*/
+	"THE SHOW GOES ON", /*7*/
+	"SUPERSTAR", /*8*/
+	"OUT OF MY HEAD", /*9*/
+	"DUMB IT DOWN", /*10*/
+	"LAMBORGHINI ANGELS", /*11*/
+	"DELIVER", /*12*/
+	"HAILE", /*13*/
+	"MISSION", /*14*/
+	"LILIES", /*15*/
+	"PROMISE", /*16*/
+	"DINOSAURS", /*17*/
+	"DINOSAURS DIET SODA POETRY", /*18*/
+	"PRECIOUS THINGS", /*19*/
+	"LF95", /*20*/
+]
